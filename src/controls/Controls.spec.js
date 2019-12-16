a93eddfa9933f25dc62cabe3f99abe8e4b5dabf7
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-16 14:26:32 -0500

    starting controls test file

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('control render', () => {
+    expect(render(<Controls/>)).toMatchSnapShot();
+});
+
+test ('unlock gate', () => {
+    const toggleLockMock = jest.fn();
+const {getByText} = render(<Controls toggleLocked={toggleLockMock} locked={true}/>)
+})
