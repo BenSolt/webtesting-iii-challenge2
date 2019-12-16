d769bade23ab25bb477c7e54b015a991001e6006
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-16 15:32:58 -0500

    added green led test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+
+import React from 'react';
+import {render} from '@testing-library/react';
+
+import Display from './Display';
+
+test('display render', () => {
+    expect(render(<Display/>)).toMatchSnapshot();
+})
+
+test('use red-led if locked', () => {
+    const {queryByText} = render(<Display locked={true}/>)
+    const locked = queryByText(/locked/i);
+    epect(locked).toHaveClass('red-led');
+});
+test('use green-led if locked', () => {
+    const {queryByText} = render(<Display locked={true}/>)
+    const locked = queryByText(/locked/i);
+    epect(locked).toHaveClass('red-led');
+});
+
